a3e15d7cfdf3d89349962a1822d874322dd1a390
Adam Sisk <adamsisk91@gmail.com>
2019-08-09 17:11:10 -0500

    moving onto bonus

--- src/index.js
+++ src/index.js
@@ -1,1 +1,68 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+const imageContainer = document
+  .getElementById('dog-image-container');
+const dogBreedContainer = document.getElementById('dog-breeds')
+const breedDropdown = document.getElementById('breed-dropdown')
+
+let allBreeds = [];
+
+dogBreedContainer.addEventListener('click', () => {
+  if (event.target.style.color === 'blue') {
+    event.target.style.color = ''
+  } else {
+    event.target.style.color = 'blue'
+  }
+})
+
+breedDropdown.addEventListener('change', () => {
+  const chosenOne = event.target.value
+  if (chosenOne == 'all') {
+    return renderBreeds(allBreeds)
+  }
+  const filteredArray = allBreeds.filter(breed => {
+    return breed[0] === chosenOne
+  })
+  renderBreeds(filteredArray)
+})
+
+const renderImages = (images) => {
+  images.forEach(image => {
+    const newImg = document.createElement('img')
+    newImg.src = image
+    imageContainer.appendChild(newImg)
+  })
+}
+
+const fetchImages = () => {
+  fetch(imgUrl)
+  .then(res => res.json())
+  .then(data => renderImages(data.message))
+}
+
+const renderBreeds = (breeds) => {
+  dogBreedContainer.innerHTML = '';
+  breeds.forEach(breed => {
+    const newLi = document.createElement('li')
+    newLi.innerText = breed
+    // newLi.addEventListener('click', () => {
+    //   newLi.style.color = 'red'
+    // })
+    dogBreedContainer.appendChild(newLi)
+  })
+}
+
+const fetchBreeds = () => {
+  fetch(breedUrl)
+    .then(res => res.json())
+    .then(data => {
+      renderBreeds(Object.keys(data.message))
+      allBreeds = Object.keys(data.message)
+    })
+}
+
+const init = () => {
+  fetchImages()
+  fetchBreeds()
+}
+init()
